8f3a3ce51b7fe05ea3236bf407e80b44bccf9217
Kevin8548 <kevinarroyolopez231@gmail.com>
2024-10-02 11:48:57 -0600

    Creacion de la clase alumno

---
+++ .history/Alumno_20241002112558.java
@@ -0,0 +1,56 @@
+public class Alumno {
+    private String matricula;
+    private String nombre;
+    private double estructuras;
+    private double ingles;
+    private double iot;
+
+    // constructor
+    public Alumno(String m, String n){
+        this.matricula = m;
+        this.nombre = n;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getEstructuras() {
+        return estructuras;
+    }
+
+    public void setEstructuras(double estructuras) {
+        this.estructuras = estructuras;
+    }
+
+    public double getIngles() {
+        return ingles;
+    }
+
+    public void setIngles(double ingles) {
+        this.ingles = ingles;
+    }
+
+    public double getIot() {
+        return iot;
+    }
+
+    public void setIot(double iot) {
+        this.iot = iot;
+    }
+
+    
+
+}
